06fa24139d7327fb36950d236e654067e38b64c4
Rushi Mashru <rushi.p.mashru@gmail.com>
2022-08-11 18:17:01 +0530

    RM: Removed EntityValue fields from BillRequestModel

--- QBFC.Models/ViewModel/BillModel.cs
+++ QBFC.Models/ViewModel/BillModel.cs
@@ -8,15 +8,15 @@ namespace QBFC.Models.ViewModel
     {
         public string RefNumber { get; set; }
         public string Vendor { get; set; }
-        public string VendorValue { get; set; }
+       // public string VendorValue { get; set; }
         public string TxnDate { get; set; }
         public string DueDate { get; set; }
         public string SalesTerm { get; set; }
-        public string SalesTermValue { get; set; }
+        // public string SalesTermValue { get; set; }
         public string ExpenseClass { get; set; }
-        public string ExpenseClassValue { get; set; }
+        // public string ExpenseClassValue { get; set; }
         public string ExpenseAccount { get; set; }
-        public string ExpenseAccountValue { get; set; }
+        // public string ExpenseAccountValue { get; set; }
         public string ExpenseDesc { get; set; }
         public string ExpenseAmount { get; set; }
         public string PrivateNote { get; set; }
